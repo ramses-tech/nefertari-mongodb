aa77e1de6413addad8999db09017cc7cbadc2c81
Artem Kostiuk <postatum@gmail.com>
2015-05-13 12:02:51 +0300

    Reload document in post_save index signal

--- nefertari_mongodb/signals.py
+++ nefertari_mongodb/signals.py
@@ -6,25 +6,22 @@ from mongoengine import signals
 log = logging.getLogger(__name__)
 
 
-def on_pre_save(sender, document, **kw):
-    from nefertari.elasticsearch import ES
-    if not kw.get('created', False) and document._get_changed_fields():
-        ES(document.__class__.__name__).index(document.to_dict())
-
-
 def on_post_save(sender, document, **kw):
+    """ Add new document to index or update existing. """
     from nefertari.elasticsearch import ES
     if kw.get('created', False):
         ES(document.__class__.__name__).index(document.to_dict())
+    elif not kw.get('created', False) and document._get_changed_fields():
+        document.reload()
+        ES(document.__class__.__name__).index(document.to_dict())
 
 
-def on_delete(sender, document, **kw):
+def on_post_delete(sender, document, **kw):
     from nefertari.elasticsearch import ES
     ES(document.__class__.__name__).delete(document.id)
 
 
 def setup_es_signals_for(source_cls):
     signals.post_save.connect(on_post_save, sender=source_cls)
-    signals.pre_save_post_validation.connect(on_pre_save, sender=source_cls)
-    signals.post_delete.connect(on_delete, sender=source_cls)
+    signals.post_delete.connect(on_post_delete, sender=source_cls)
     log.info('setup_es_signals_for: %r' % source_cls)
