4c99de882012cb0fd123f08fb06046dba41273f5
Artem Kostiuk <postatum@gmail.com>
2015-05-27 12:17:29 +0300

    Pass refresh_index from signals to ES

--- nefertari_mongodb/signals.py
+++ nefertari_mongodb/signals.py
@@ -10,17 +10,22 @@ log = logging.getLogger(__name__)
 def on_post_save(sender, document, **kw):
     """ Add new document to index or update existing. """
     from nefertari.elasticsearch import ES
+    refresh_index = getattr(document, '_refresh_index', False)
     created = kw.get('created', False)
     if created:
-        ES(document.__class__.__name__).index(document.to_dict())
+        ES(document.__class__.__name__).index(
+            document.to_dict(), refresh_index=refresh_index)
     elif not created and document._get_changed_fields():
         document.reload()
-        ES(document.__class__.__name__).index(document.to_dict())
+        ES(document.__class__.__name__).index(
+            document.to_dict(), refresh_index=refresh_index)
 
 
 def on_post_delete(sender, document, **kw):
     from nefertari.elasticsearch import ES
-    ES(document.__class__.__name__).delete(document.id)
+    refresh_index = getattr(document, '_refresh_index', False)
+    ES(document.__class__.__name__).delete(
+        document.id, refresh_index=refresh_index)
 
 
 def on_bulk_update(model_cls, objects, refresh_index=False):
@@ -33,11 +38,11 @@ def on_bulk_update(model_cls, objects, refresh_index=False):
     from nefertari.elasticsearch import ES
     es = ES(source=model_cls.__name__)
     documents = to_dicts(objects)
-    es.index(documents)
+    es.index(documents, refresh_index=refresh_index)
 
     # Reindex relationships
     for obj in objects:
-        es.index_refs(obj)
+        es.index_refs(obj, refresh_index=refresh_index)
 
 
 def setup_es_signals_for(source_cls):
