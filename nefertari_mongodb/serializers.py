b4ce232f050de073572f64c04b170a2e790fdc24
Artem Kostiuk <postatum@gmail.com>
2015-07-10 13:40:14 +0300

    Refactor encoders to have base class

--- nefertari_mongodb/serializers.py
+++ nefertari_mongodb/serializers.py
@@ -11,39 +11,33 @@ from nefertari.renderers import _JSONEncoder
 log = logging.getLogger(__name__)
 
 
-class JSONEncoder(_JSONEncoder):
+class JSONEncoderMixin(object):
     def default(self, obj):
         if isinstance(obj, (ObjectId, DBRef)):
             return str(obj)
-        if isinstance(obj, decimal.Decimal):
-            return float(obj)
         if isinstance(obj, (datetime.datetime, datetime.date)):
             return obj.strftime("%Y-%m-%dT%H:%M:%SZ")  # iso
         if isinstance(obj, datetime.time):
             return obj.strftime('%H:%M:%S')
         if isinstance(obj, datetime.timedelta):
             return obj.seconds
+        if isinstance(obj, decimal.Decimal):
+            return float(obj)
+        return super(JSONEncoderMixin, self).default(obj)
 
+
+class JSONEncoder(JSONEncoderMixin, _JSONEncoder):
+    def default(self, obj):
         if hasattr(obj, 'to_dict'):
             # If it got to this point, it means its a nested object.
             # outter objects would have been handled with DataProxy.
             return obj.to_dict(__nested=True)
-
         return super(JSONEncoder, self).default(obj)
 
 
-class ESJSONSerializer(elasticsearch.serializer.JSONSerializer):
+class ESJSONSerializer(JSONEncoderMixin,
+                       elasticsearch.serializer.JSONSerializer):
     def default(self, obj):
-        if isinstance(obj, (ObjectId, DBRef)):
-            return str(obj)
-        if isinstance(obj, (datetime.datetime, datetime.date)):
-            return obj.strftime("%Y-%m-%dT%H:%M:%SZ")  # iso
-        if isinstance(obj, datetime.time):
-            return obj.strftime('%H:%M:%S')
-        if isinstance(obj, datetime.timedelta):
-            return obj.seconds
-        if isinstance(obj, decimal.Decimal):
-            return float(obj)
         try:
             return super(ESJSONSerializer, self).default(obj)
         except:
